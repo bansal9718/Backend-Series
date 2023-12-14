f4e8c2e984f345710fa06e892a67c7ec8634dd18
Rohit <rohitbansal4685@gmail.com>
2023-12-14 21:30:21 +0530

    user and video model and hooks

---
+++ src/models/video.model.js
@@ -0,0 +1,45 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new mongoose.Schema(
+  {
+    videoFile: {
+      type: String, //cloudinaryUrl
+      required: true,
+    },
+    thumbnail: {
+      type: String, //cloudinaryUrl
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number, //cloudinaryUrl
+      required: true,
+    },
+
+    views: {
+      type: Number,
+      default: 0,
+    },
+
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate);
+export const video = mongoose.model("Video", videoSchema);
