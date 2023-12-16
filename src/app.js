90ce5be4b3f6b6a91b19c1864a4a44fc941a6448
Rohit <rohitbansal4685@gmail.com>
2023-12-16 11:05:08 +0530

    User registration and routing

--- src/app.js
+++ src/app.js
@@ -9,4 +9,11 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+//routes
+import userRouter from "./routes/user.route.js";
+
+//routes decalaration
+app.use("/api/v1/users", userRouter);
+
+//http://localhost:8000/api/v1/users/register
 export default app;
