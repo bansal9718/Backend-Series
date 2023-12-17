68930d947002b5037b500a9626af43be803ba5b7
Rohit <rohitbansal4685@gmail.com>
2023-12-17 09:05:51 +0530

    small change

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -130,7 +130,7 @@ const loginUser = asyncHandler(async (req, res) => {
 });
 
 const logoutUser = await asyncHandler(async (req, res) => {
-  User.findByIdAndUpdate(
+  await User.findByIdAndUpdate(
     req.user._id,
     {
       $set: {
